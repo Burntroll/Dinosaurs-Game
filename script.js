055125c7585f6d50724087f505e3db941a58da22
Burntroll <francisco.silva.ru@gmail.com>
2022-09-09 23:10:54 +0300

    Criacao da dinamica do cactus

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
 
 function jump() {
     let position = 0;
+
+    isJumping = true;
+
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
@@ -16,6 +24,7 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 20) {
                     clearInterval(downInterval);
+                    isJumping = false;
                 }
                 position -= 20;
                 dino.style.bottom = position + "px";
@@ -28,5 +37,29 @@ function jump() {
             }
     }, 20)
 }
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 5000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px'; 
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime)
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
